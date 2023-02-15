2a423facf59898b6277e44daa12937f962749850
2081518 <thor.marinho11@yahoo.com>
2023-02-15 20:11:39 -0300

    Simples correções no cabeçalho do relatorio, clean coding no extract_worms_data, tabela de correlação criada, adicionados arquivos que não devem estar sendo trackeados no .gitignore e a biblioteca corrplot foi adicionada ao requirements, ja que ela está sendo utilizada para a construção da tabela de correlação

--- extract_worms_data.R
+++ extract_worms_data.R
@@ -3,10 +3,10 @@ library("XML")
 #library("tidyverse")
 library("rvest")
 
-  nadolny = "Nadolny_etal_Worms_Brazil_DRYAD_9.xlsx"
-  data_dryad_url <- "https://datadryad.org/stash/dataset/doi:10.5061%2Fdryad.4md0s64"
-  data_dryad_html <- rvest::read_html(data_dryad_url)
-  
-  data_worms_url <- paste0("https://datadryad.org", html_text(html_nodes(data_dryad_html, xpath=paste0("//a[@title='", nadolny, "']/@href"))))
-  
-  download.file(url = data_worms_url, destfile = nadolny, mode="wb")
+nadolny = "Nadolny_etal_Worms_Brazil_DRYAD_9.xlsx"
+data_dryad_url <- "https://datadryad.org/stash/dataset/doi:10.5061%2Fdryad.4md0s64"
+data_dryad_html <- rvest::read_html(data_dryad_url)
+
+data_worms_url <- paste0("https://datadryad.org", html_text(html_nodes(data_dryad_html, xpath=paste0("//a[@title='", nadolny, "']/@href"))))
+
+download.file(url = data_worms_url, destfile = nadolny, mode="wb")
